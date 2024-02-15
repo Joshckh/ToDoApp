619982e59fd42f6c0fc7873bb06a04d393b48be3
Joshckh <105426491+Joshckh@users.noreply.github.com>
2024-02-15 15:45:54 +0800

    Delete src/src directory

--- src/src/js/task.js
+++
@@ -1,9 +0,0 @@
-export class Task {
-    constructor(name,about,deadline,priority){
-        this.name = name;
-        this.about = about;
-        this.deadline = deadline;
-        this.priority = priority;
-        this.completed = false
-    }
-}
